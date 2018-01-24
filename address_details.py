509a7204fdeb2e008e7910ebb97f1625a3aee1b0
dholness <dholness@thoughtworks.com>
2018-01-23 20:29:51 -0500

    set named tulpe verbose to false

--- address_details.py
+++ address_details.py
@@ -1,3 +1,3 @@
 from collections import namedtuple
 
-AddressDetails = namedtuple('AddressDetails', ['address', 'balance', 'transactions'], verbose=True)
+AddressDetails = namedtuple('AddressDetails', ['address', 'balance', 'transactions'], verbose=False)
